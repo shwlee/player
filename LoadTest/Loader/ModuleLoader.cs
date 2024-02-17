e57429a5bbf40bc97f835400387f6bb99bb285e3
shwlee <vactormanbear@gmail.com>
2024-02-17 23:54:43 +0900

    tester 모듈 프로젝트.
    load test 추가.

---
+++ LoadTest/Loader/ModuleLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace LoadTest.Loader;
+
+internal class ModuleLoader
+{
+	public static void TestJsModule()
+	{
+		var path = "../../../../JsPlayer/bin/Debug/netstandard2.1/JsPlayer.dll";
+		var jsAssembly = Assembly.LoadFrom(path);
+		var stream = jsAssembly.GetManifestResourceStream("JsPlayer.cloPlayer.js");
+		using (var reader = new StreamReader(stream))
+		{
+			Console.WriteLine(reader.ReadToEnd());
+		}
+	}
+}
---
+++ LoadTest/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LoadTest.Loader;
+
+Console.WriteLine("load test start!");
+
+ModuleLoader.TestJsModule();
+
+Console.WriteLine("DONE!");
