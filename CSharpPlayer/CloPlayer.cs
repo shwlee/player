77baa096e586936468908ebc4f9261c2d7673e58
shwlee <vactormanbear@gmail.com>
2024-03-03 15:53:47 +0900

    IPlayer 참조 제거.

--- CSharpPlayer/CloPlayer.cs
+++ CSharpPlayer/CloPlayer.cs
@@ -1,13 +1,12 @@
-using PlayerLib;
 using System;
 
 namespace CSharpPlayer
 {
 	/// <summary>
 	/// sample player 입니다. 이 클래스에 추가 작성해도 좋고 별도의 프로젝트에 다시 작성해도 무방합니다.
-	/// <para>클래스 이름에 특별한 제한은 없습니다. 접근제한자를 public 으로 지정해주세요.</para>
+	/// <para>필수 메서드 GetName(), Initialize(), MoveNext()의 접근제한자를 public 으로 지정해주세요.</para>
 	/// </summary>
-	public class CloPlayer : IPlayer
+	public class CloPlayer
 	{
 		private int _myNumber;
 		private int _column;
