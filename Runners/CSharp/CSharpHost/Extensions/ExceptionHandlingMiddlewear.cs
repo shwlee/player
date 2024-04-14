2e143c5ce67578f99d0fd484d0ec3fe0832eb6c1
shwlee <vactormanbear@gmail.com>
2024-04-14 15:50:28 +0900

    예외 메시지 강화.

--- Runners/CSharp/CSharpHost/Extensions/ExceptionHandlingMiddlewear.cs
+++ Runners/CSharp/CSharpHost/Extensions/ExceptionHandlingMiddlewear.cs
@@ -13,20 +13,29 @@ public class ExceptionHandlingMiddlewear(RequestDelegate next, ILogger<Exception
         {
             await _next(context);
         }
-        catch (Exception exception)
+        catch (Exception ex)
         {
-            _logger.LogError(
-                exception, "Exception occurred: {Message}", exception.Message);
+            _logger.LogError(ex, "Exception occurred: {Message}", ex.Message);
 
             var problemDetails = new ProblemDetails
             {
-                Status = StatusCodes.Status500InternalServerError,                
-                Detail = exception.Message
+                Status = StatusCodes.Status500InternalServerError,
+                Detail = GetErrorMessage(ex)
             };
 
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
 
             await context.Response.WriteAsJsonAsync(problemDetails);
         }
+    }
+
+    private string GetErrorMessage(Exception ex)
+    {
+        if (ex.InnerException is null)
+        {
+            return ex.Message;
+        }
+
+        return $"{ex.Message} - {ex.InnerException.Message}";
     }
 }
