35efd76694a7987337afbcc6d5e40560b08b12cd
shwlee <vactormanbear@gmail.com>
2024-06-12 21:17:31 +0900

    버그 수정 및 기타 개선 사항 추가.
    - router 로 부터 숫자형식이 문자로 전달되는 부분을 모두 숫자로 수정.
    - players 객체 구조 수정.(filePath 를 함께 보관하도록 지정)
    - cleanup 때 require() 캐쉬 제거

--- Runners/Javascript/JsHost/services/gameService.js
+++ Runners/Javascript/JsHost/services/gameService.js
@@ -6,8 +6,8 @@ class GameService {
 	}
 
 	setGame(column, row) {
-		this._column = column;
-		this._row = row;
+		this._column = Number(column);
+		this._row = Number(row);
 	}
 
 	getGame() {
@@ -19,37 +19,43 @@ class GameService {
 
 	loadPlayer(position, filePath) {
 		try {
+			position = Number(position);
 			const runner = require(filePath); // A.js 모듈 로드
 			const instance = new runner(); // a1 클래스의 인스턴스 생성
-			this._players[position] = instance;
+			this._players[position] ={
+				filePath,
+				player : instance
+			};
 			return 200;
 		}
 		catch (error) {
 			// log
-			console.log(error);
+			console.log("loadPlayer error", error);
 			return 400; // 실패 시 400 에러로 반환
 		}
 	}
 
 	getPlayerName(position) {
 		try {
-			const player = this._players[position];
+			position = Number(position);
+			const { filePath, player } = this._players[position];			
 			return player.getName();
 		}
 		catch (error) {
-			console.log(`error! ${error}`)
+			console.log("getPlayerName error", error);
 			return error;
 		}
 	}
 
 	initPlayer(position, column, row) {
 		try {
-			const player = this._players[position];
+			position = Number(position);
+			const { filePath, player } = this._players[position];			
 			player.initialize(position, column, row);
 			return 200;
 		} catch (error) {
 			// log	
-			console.log(error);
+			console.log("initPlayer error", error);
 			return 400;
 		}
 	}
@@ -57,12 +63,14 @@ class GameService {
 	moveNext(gameContext) {
 		try {
 			const { current, map, position } = gameContext;
-			const player = this._players[position.toString()];
+			const playerPosition = Number(position);
+			const { filePath, player } = this._players[playerPosition];			
 			if (player === undefined) {
 				return -1;
 			}
 			return player.moveNext(map, current);
 		} catch (error) {
+			console.log("moveNext", error);
 			return -1;
 		}
 	}
@@ -71,8 +79,19 @@ class GameService {
 		try {
 			this._column = 0;
 			this._row = 0;
+
+			console.log("start cleanup!");
+			// delete require() cache 
+			for(const index in this._players)	{
+				const { filePath, player } = this._players[index];				
+				delete require.cache[require.resolve(filePath)];
+				console.log("delete cache", filePath);
+			}
+
 			this._players = {}
+			console.log("cleanup complated!");
 		} catch (error) {
+			console.log("cleanup error", error);
 			return -1;
 		}
 	}
