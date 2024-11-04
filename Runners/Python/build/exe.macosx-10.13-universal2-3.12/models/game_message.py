57f479d61b3c264c48d5ff6090a6c4a5a4a76a60
Ian-kim2012 <121046114+thenewth@users.noreply.github.com>
2024-11-05 00:39:43 +0900

    delete exefiles

--- Runners/Python/build/exe.macosx-10.13-universal2-3.12/models/game_message.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class GameMessage(BaseModel):
-    position: int
-    map: List[int]
-    current: int
--- Runners/Python/build/exe.win-amd64-3.12/models/game_set.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class GameSet(BaseModel):
-    column: int
-    row: int
