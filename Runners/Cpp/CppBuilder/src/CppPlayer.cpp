a6c469474f7c2b2f2cae0ca0ee90d27f6deccd47
Aaron Kim <aaron@clo3d.com>
2024-11-05 20:45:30 +0900

    Cpp Player dll builder 추가

---
+++ Runners/Cpp/CppBuilder/src/CppPlayer.cpp
@@ -0,0 +1,15 @@
+#include "CppPlayer.h"
+
+void CCppPlayer::Initialize(int myNumber, int column, int row)
+{
+}
+
+const char* CCppPlayer::GetName()
+{
+    return "Faker11";
+}
+
+int CCppPlayer::MoveNext(int argc, int* argv, int myPosition)
+{
+    return 0;
+}
