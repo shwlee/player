fc7d512356bb90627b6ee8c08e1e62bf03027872
Ian-kim2012 <121046114+thenewth@users.noreply.github.com>
2024-11-12 00:35:21 +0900

    check endpoint query

--- Runners/Python/PyHost/routes/game.py
+++ Runners/Python/PyHost/routes/game.py
@@ -23,9 +23,9 @@ async def healthy():
     return JSONResponse(content="Healthy", status_code=200)
 
 @router.post("/set")
-async def set_game(column: int = Query(...), row: int = Query(...), game_service: GameService = Depends(get_game_service)):
+async def set_game(gameId: str = Query(...), column: int = Query(...), row: int = Query(...), game_service: GameService = Depends(get_game_service)):
     try:
-        game_service.init_game(column, row)
+        game_service.init_game(gameId, column, row)
         return JSONResponse(content="Game set successfully", status_code=200)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
